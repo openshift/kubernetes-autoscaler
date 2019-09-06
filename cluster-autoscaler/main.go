76894dc37a92f881abf5645b305bd6b3c86ee444
Andrew McDermott <amcdermo@redhat.com>
2019-09-06 16:25:17 +0100

    UPSTREAM: <carry>: openshift: revert https://github.com/openshift/kubernetes-autoscaler/pull/113
    This reverts commit b8ec486e1f45e2f8dca8d8c208f1968c3b23de6f.

--- cluster-autoscaler/main.go
+++ cluster-autoscaler/main.go
@@ -35,7 +35,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider"
 	cloudBuilder "k8s.io/autoscaler/cluster-autoscaler/cloudprovider/builder"
-	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/openshiftmachineapi"
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 	"k8s.io/autoscaler/cluster-autoscaler/core"
 	"k8s.io/autoscaler/cluster-autoscaler/estimator"
@@ -295,15 +294,6 @@ func buildAutoscaler() (core.Autoscaler, error) {
 	if autoscalingOptions.CloudProviderName == cloudprovider.AzureProviderName {
 		processors.NodeGroupSetProcessor = &nodegroupset.BalancingNodeGroupSetProcessor{
 			Comparator: nodegroupset.IsAzureNodeInfoSimilar}
-	}
-
-	if autoscalingOptions.CloudProviderName == openshiftmachineapi.ProviderName {
-		// We do this because of:
-		// - https://bugzilla.redhat.com/show_bug.cgi?id=1731011
-		// - https://bugzilla.redhat.com/show_bug.cgi?id=1733235
-		processors.NodeGroupSetProcessor = &nodegroupset.BalancingNodeGroupSetProcessor{
-			Comparator: nodegroupset.IsOpenShiftMachineApiNodeInfoSimilar}
-
 	}
 
 	opts := core.AutoscalerOptions{
