038fd128d53577637ef1cfcd2d744fb9fc05fc48
Andrew McDermott <amcdermo@redhat.com>
2019-09-12 19:23:20 +0100

    UPSTREAM: <carry>: openshift: plumb cloudprovider/u
    Add cloudprovider/u to the available set of cloud providers. The
    existing cloudproviders/openshiftmacineapi is still present but
    specifying `--cloud-provider="openshift-machine-api"` will now use
    cloudproviders/u instead.

--- cluster-autoscaler/cloudprovider/builder/builder_all.go
+++ cluster-autoscaler/cloudprovider/builder/builder_all.go
@@ -1,4 +1,4 @@
-// +build !gce,!aws,!azure,!kubemark,!alicloud,!openshiftmachineapi
+// +build !gce,!aws,!azure,!kubemark,!alicloud,!openshiftmachineapi,!u
 
 /*
 Copyright 2018 The Kubernetes Authors.
@@ -27,6 +27,7 @@ import (
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gce"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/gke"
 	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/openshiftmachineapi"
+	"k8s.io/autoscaler/cluster-autoscaler/cloudprovider/u"
 	"k8s.io/autoscaler/cluster-autoscaler/config"
 )
 
@@ -39,6 +40,7 @@ var AvailableCloudProviders = []string{
 	alicloud.ProviderName,
 	baiducloud.ProviderName,
 	openshiftmachineapi.ProviderName,
+	u.ProviderName,
 }
 
 // DefaultCloudProvider is GCE.
@@ -58,8 +60,8 @@ func buildCloudProvider(opts config.AutoscalingOptions, do cloudprovider.NodeGro
 		return alicloud.BuildAlicloud(opts, do, rl)
 	case baiducloud.ProviderName:
 		return baiducloud.BuildBaiducloud(opts, do, rl)
-	case openshiftmachineapi.ProviderName:
-		return openshiftmachineapi.BuildOpenShiftMachineAPI(opts, do, rl)
+	case u.ProviderName:
+		return u.BuildOpenShiftMachineAPI(opts, do, rl)
 	}
 	return nil
 }
