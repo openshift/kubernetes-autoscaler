13f2fa846edb4a82a3cc98946fdb63eb4cf1a49c
Enxebre <alberto.garcial@hotmail.com>
2020-02-27 17:51:45 +0100

    UPSTREAM: <carry>: openshift: cleanup machine API types

--- cluster-autoscaler/cloudprovider/openshiftmachineapi/machine_types.go
+++ cluster-autoscaler/cloudprovider/openshiftmachineapi/machine_types.go
@@ -17,30 +17,11 @@ limitations under the License.
 package openshiftmachineapi
 
 import (
-	"fmt"
-
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
-const (
-	// MachineClusterLabelName is the label set on machines linked to a cluster.
-	MachineClusterLabelName = "cluster.k8s.io/cluster-name"
-
-	// MachineClusterIDLabel is the label that a machine must have to identify the
-	// cluster to which it belongs.
-	MachineClusterIDLabel = "machine.openshift.io/cluster-api-cluster"
-)
-
-// +genclient
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-/// [Machine]
 // Machine is the Schema for the machines API
-// +k8s:openapi-gen=true
-// +kubebuilder:subresource:status
 type Machine struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -49,9 +30,6 @@ type Machine struct {
 	Status MachineStatus `json:"status,omitempty"`
 }
 
-/// [Machine]
-
-/// [MachineSpec]
 // MachineSpec defines the desired state of Machine
 type MachineSpec struct {
 	// ObjectMeta will autopopulate the Node created. Use this to
@@ -65,10 +43,6 @@ type MachineSpec struct {
 	// an ongoing basis.
 	// +optional
 	Taints []corev1.Taint `json:"taints,omitempty"`
-
-	// ProviderSpec details Provider-specific configuration to use during node creation.
-	// +optional
-	ProviderSpec ProviderSpec `json:"providerSpec"`
 
 	// ProviderID is the identification ID of the machine provided by the provider.
 	// This field must match the provider ID as seen on the node object corresponding to this machine.
@@ -84,118 +58,12 @@ type MachineSpec struct {
 	ProviderID *string `json:"providerID,omitempty"`
 }
 
-/// [MachineSpec]
-
-/// [MachineStatus]
 // MachineStatus defines the observed state of Machine
 type MachineStatus struct {
 	// NodeRef will point to the corresponding Node if it exists.
 	// +optional
 	NodeRef *corev1.ObjectReference `json:"nodeRef,omitempty"`
-
-	// LastUpdated identifies when this status was last observed.
-	// +optional
-	LastUpdated *metav1.Time `json:"lastUpdated,omitempty"`
-
-	// ErrorReason will be set in the event that there is a terminal problem
-	// reconciling the Machine and will contain a succinct value suitable
-	// for machine interpretation.
-	//
-	// This field should not be set for transitive errors that a controller
-	// faces that are expected to be fixed automatically over
-	// time (like service outages), but instead indicate that something is
-	// fundamentally wrong with the Machine's spec or the configuration of
-	// the controller, and that manual intervention is required. Examples
-	// of terminal errors would be invalid combinations of settings in the
-	// spec, values that are unsupported by the controller, or the
-	// responsible controller itself being critically misconfigured.
-	//
-	// Any transient errors that occur during the reconciliation of Machines
-	// can be added as events to the Machine object and/or logged in the
-	// controller's output.
-	// +optional
-	//ErrorReason *common.MachineStatusError `json:"errorReason,omitempty"`
-
-	// ErrorMessage will be set in the event that there is a terminal problem
-	// reconciling the Machine and will contain a more verbose string suitable
-	// for logging and human consumption.
-	//
-	// This field should not be set for transitive errors that a controller
-	// faces that are expected to be fixed automatically over
-	// time (like service outages), but instead indicate that something is
-	// fundamentally wrong with the Machine's spec or the configuration of
-	// the controller, and that manual intervention is required. Examples
-	// of terminal errors would be invalid combinations of settings in the
-	// spec, values that are unsupported by the controller, or the
-	// responsible controller itself being critically misconfigured.
-	//
-	// Any transient errors that occur during the reconciliation of Machines
-	// can be added as events to the Machine object and/or logged in the
-	// controller's output.
-	// +optional
-	ErrorMessage *string `json:"errorMessage,omitempty"`
-
-	// ProviderStatus details a Provider-specific status.
-	// It is recommended that providers maintain their
-	// own versioned API types that should be
-	// serialized/deserialized from this field.
-	// +optional
-	ProviderStatus *runtime.RawExtension `json:"providerStatus,omitempty"`
-
-	// Addresses is a list of addresses assigned to the machine. Queried from cloud provider, if available.
-	// +optional
-	Addresses []corev1.NodeAddress `json:"addresses,omitempty"`
-
-	// LastOperation describes the last-operation performed by the machine-controller.
-	// This API should be useful as a history in terms of the latest operation performed on the
-	// specific machine. It should also convey the state of the latest-operation for example if
-	// it is still on-going, failed or completed successfully.
-	// +optional
-	LastOperation *LastOperation `json:"lastOperation,omitempty"`
-
-	// Phase represents the current phase of machine actuation.
-	// E.g. Pending, Running, Terminating, Failed etc.
-	// +optional
-	Phase *string `json:"phase,omitempty"`
 }
-
-// LastOperation represents the detail of the last performed operation on the MachineObject.
-type LastOperation struct {
-	// Description is the human-readable description of the last operation.
-	Description *string `json:"description,omitempty"`
-
-	// LastUpdated is the timestamp at which LastOperation API was last-updated.
-	LastUpdated *metav1.Time `json:"lastUpdated,omitempty"`
-
-	// State is the current status of the last performed operation.
-	// E.g. Processing, Failed, Successful etc
-	State *string `json:"state,omitempty"`
-
-	// Type is the type of operation which was last performed.
-	// E.g. Create, Delete, Update etc
-	Type *string `json:"type,omitempty"`
-}
-
-/// [MachineVersionInfo]
-
-func (m *Machine) Validate() field.ErrorList {
-	errors := field.ErrorList{}
-
-	// validate spec.labels
-	fldPath := field.NewPath("spec")
-	if m.Labels[MachineClusterIDLabel] == "" {
-		errors = append(errors, field.Invalid(fldPath.Child("labels"), m.Labels, fmt.Sprintf("missing %v label.", MachineClusterIDLabel)))
-	}
-
-	// validate provider config is set
-	if m.Spec.ProviderSpec.Value == nil {
-		errors = append(errors, field.Invalid(fldPath.Child("spec").Child("providerspec"), m.Spec.ProviderSpec, "value field must be set"))
-	}
-
-	return errors
-}
-
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
 // MachineList contains a list of Machine
 type MachineList struct {
@@ -203,7 +71,3 @@ type MachineList struct {
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []Machine `json:"items"`
 }
-
-// func init() {
-// 	SchemeBuilder.Register(&Machine{}, &MachineList{})
-// }
--- cluster-autoscaler/cloudprovider/openshiftmachineapi/machineset_types.go
+++ cluster-autoscaler/cloudprovider/openshiftmachineapi/machineset_types.go
@@ -17,23 +17,10 @@ limitations under the License.
 package openshiftmachineapi
 
 import (
-	"log"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	metav1validation "k8s.io/apimachinery/pkg/apis/meta/v1/validation"
-
-	"k8s.io/apimachinery/pkg/labels"
-	"k8s.io/apimachinery/pkg/util/validation/field"
 )
 
-// +genclient
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-/// [MachineSet]
-// MachineSet ensures that a specified number of machines replicas are running at any given time.
-// +k8s:openapi-gen=true
-// +kubebuilder:subresource:status
-// +kubebuilder:subresource:scale:specpath=.spec.replicas,statuspath=.status.replicas,selectorpath=.status.labelSelector
+// MachineSet is the internal autoscaler Schema for machineSets
 type MachineSet struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -42,10 +29,7 @@ type MachineSet struct {
 	Status MachineSetStatus `json:"status,omitempty"`
 }
 
-/// [MachineSet]
-
-/// [MachineSetSpec]
-// MachineSetSpec defines the desired state of MachineSet
+// MachineSetSpec is the internal autoscaler Schema for MachineSetSpec
 type MachineSetSpec struct {
 	// Replicas is the number of desired replicas.
 	// This is a pointer to distinguish between explicit zero and unspecified.
@@ -70,10 +54,7 @@ type MachineSetSpec struct {
 	Template MachineTemplateSpec `json:"template,omitempty"`
 }
 
-/// [MachineSetSpec] // doxygen marker
-
-/// [MachineTemplateSpec] // doxygen marker
-// MachineTemplateSpec describes the data needed to create a Machine from a template
+// MachineTemplateSpec is the internal autoscaler Schema for MachineTemplateSpec
 type MachineTemplateSpec struct {
 	// Standard object's metadata.
 	// More info: https://git.k8s.io/community/contributors/devel/api-conventions.md#metadata
@@ -86,101 +67,12 @@ type MachineTemplateSpec struct {
 	Spec MachineSpec `json:"spec,omitempty"`
 }
 
-/// [MachineTemplateSpec]
+// MachineSetStatus is the internal autoscaler Schema for MachineSetStatus
+type MachineSetStatus struct{}
 
-/// [MachineSetStatus]
-// MachineSetStatus defines the observed state of MachineSet
-type MachineSetStatus struct {
-	// Replicas is the most recently observed number of replicas.
-	Replicas int32 `json:"replicas"`
-
-	// The number of replicas that have labels matching the labels of the machine template of the MachineSet.
-	// +optional
-	FullyLabeledReplicas int32 `json:"fullyLabeledReplicas,omitempty"`
-
-	// The number of ready replicas for this MachineSet. A machine is considered ready when the node has been created and is "Ready".
-	// +optional
-	ReadyReplicas int32 `json:"readyReplicas,omitempty"`
-
-	// The number of available replicas (ready for at least minReadySeconds) for this MachineSet.
-	// +optional
-	AvailableReplicas int32 `json:"availableReplicas,omitempty"`
-
-	// ObservedGeneration reflects the generation of the most recently observed MachineSet.
-	// +optional
-	ObservedGeneration int64 `json:"observedGeneration,omitempty"`
-
-	// In the event that there is a terminal problem reconciling the
-	// replicas, both ErrorReason and ErrorMessage will be set. ErrorReason
-	// will be populated with a succinct value suitable for machine
-	// interpretation, while ErrorMessage will contain a more verbose
-	// string suitable for logging and human consumption.
-	//
-	// These fields should not be set for transitive errors that a
-	// controller faces that are expected to be fixed automatically over
-	// time (like service outages), but instead indicate that something is
-	// fundamentally wrong with the MachineTemplate's spec or the configuration of
-	// the machine controller, and that manual intervention is required. Examples
-	// of terminal errors would be invalid combinations of settings in the
-	// spec, values that are unsupported by the machine controller, or the
-	// responsible machine controller itself being critically misconfigured.
-	//
-	// Any transient errors that occur during the reconciliation of Machines
-	// can be added as events to the MachineSet object and/or logged in the
-	// controller's output.
-	// +optional
-	//ErrorReason *common.MachineSetStatusError `json:"errorReason,omitempty"`
-	// +optional
-	ErrorMessage *string `json:"errorMessage,omitempty"`
-}
-
-/// [MachineSetStatus]
-
-func (m *MachineSet) Validate() field.ErrorList {
-	errors := field.ErrorList{}
-
-	// validate spec.selector and spec.template.labels
-	fldPath := field.NewPath("spec")
-	errors = append(errors, metav1validation.ValidateLabelSelector(&m.Spec.Selector, fldPath.Child("selector"))...)
-	if len(m.Spec.Selector.MatchLabels)+len(m.Spec.Selector.MatchExpressions) == 0 {
-		errors = append(errors, field.Invalid(fldPath.Child("selector"), m.Spec.Selector, "empty selector is not valid for MachineSet."))
-	}
-	selector, err := metav1.LabelSelectorAsSelector(&m.Spec.Selector)
-	if err != nil {
-		errors = append(errors, field.Invalid(fldPath.Child("selector"), m.Spec.Selector, "invalid label selector."))
-	} else {
-		labels := labels.Set(m.Spec.Template.Labels)
-		if !selector.Matches(labels) {
-			errors = append(errors, field.Invalid(fldPath.Child("template", "metadata", "labels"), m.Spec.Template.Labels, "`selector` does not match template `labels`"))
-		}
-	}
-
-	return errors
-}
-
-// DefaultingFunction sets default MachineSet field values
-func (m *MachineSet) Default() {
-	log.Printf("Defaulting fields for MachineSet %s\n", m.Name)
-
-	if m.Spec.Replicas == nil {
-		m.Spec.Replicas = new(int32)
-		*m.Spec.Replicas = 1
-	}
-
-	if len(m.Namespace) == 0 {
-		m.Namespace = metav1.NamespaceDefault
-	}
-}
-
-// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
-
-// MachineSetList contains a list of MachineSet
+// MachineSetList is the internal autoscaler Schema for MachineSetList
 type MachineSetList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []MachineSet `json:"items"`
 }
-
-// func init() {
-// 	SchemeBuilder.Register(&MachineSet{}, &MachineSetList{})
-// }
