5da13d43e63b9db57c4d33103ad996ac24c7a2be
Michael McCune <elmiko@redhat.com>
2022-09-15 16:50:57 -0400

    UPSTREAM: <drop>: revert capacity annotations
    the upstream annotations for the scale from zero capacity resources is
    slighty different than the openshift implementation. the largest
    difference is the addition of a gpu type annotation. openshift does not
    yet utilize this annotation and thus this patch should be carried until
    the machineset controllers for the various providers on openshift have
    been modified to use the new annotations.
    
    another important change is the modification of the memory annotation.
    previously in openshift we expected this value to be a count of memory
    in Mebibytes. the conversion function and tests have been modified to
    allow continued openshift operation.
    
    this change can be dropped when the annotations in openshift have been
    updated, the progress for this effort can be followed at
    https://issues.redhat.com/browse/OCPCLOUD-944

--- cluster-autoscaler/cloudprovider/clusterapi/clusterapi_unstructured_test.go
+++ cluster-autoscaler/cloudprovider/clusterapi/clusterapi_unstructured_test.go
@@ -25,6 +25,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/autoscaler/cluster-autoscaler/utils/units"
 	"k8s.io/client-go/tools/cache"
 )
 
@@ -283,7 +284,7 @@ func TestSetSizeAndReplicas(t *testing.T) {
 
 func TestAnnotations(t *testing.T) {
 	cpuQuantity := resource.MustParse("2")
-	memQuantity := resource.MustParse("1024Mi")
+	memQuantity := resource.MustParse("1024")
 	gpuQuantity := resource.MustParse("1")
 	maxPodsQuantity := resource.MustParse("42")
 	annotations := map[string]string{
@@ -292,6 +293,11 @@ func TestAnnotations(t *testing.T) {
 		gpuCountKey: gpuQuantity.String(),
 		maxPodsKey:  maxPodsQuantity.String(),
 	}
+
+	// convert the initial memory value from Mebibytes to bytes as this conversion happens internally
+	// when we use InstanceMemoryCapacity()
+	memVal, _ := memQuantity.AsInt64()
+	memQuantityAsBytes := resource.NewQuantity(memVal*units.MiB, resource.DecimalSI)
 
 	test := func(t *testing.T, testConfig *testConfig, testResource *unstructured.Unstructured) {
 		controller, stop := mustCreateTestController(t, testConfig)
@@ -310,7 +316,7 @@ func TestAnnotations(t *testing.T) {
 
 		if mem, err := sr.InstanceMemoryCapacityAnnotation(); err != nil {
 			t.Fatal(err)
-		} else if memQuantity.Cmp(mem) != 0 {
+		} else if memQuantityAsBytes.Cmp(mem) != 0 {
 			t.Errorf("expected %v, got %v", memQuantity, mem)
 		}
 
@@ -349,7 +355,7 @@ func TestCanScaleFromZero(t *testing.T) {
 			"can scale from zero",
 			map[string]string{
 				cpuKey:    "1",
-				memoryKey: "1024Mi",
+				memoryKey: "1024",
 			},
 			nil,
 			true,
@@ -357,7 +363,7 @@ func TestCanScaleFromZero(t *testing.T) {
 		{
 			"with missing CPU info cannot scale from zero",
 			map[string]string{
-				memoryKey: "1024Mi",
+				memoryKey: "1024",
 			},
 			nil,
 			false,
@@ -405,7 +411,7 @@ func TestCanScaleFromZero(t *testing.T) {
 			"with both annotations and capacity in machine template can scale from zero",
 			map[string]string{
 				cpuKey:    "1",
-				memoryKey: "1024Mi",
+				memoryKey: "1024",
 			},
 			map[string]string{
 				cpuStatusKey:    "1",
