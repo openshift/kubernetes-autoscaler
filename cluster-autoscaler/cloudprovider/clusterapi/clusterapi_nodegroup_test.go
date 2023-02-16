d1be3e33372180492cb0bf51f95bc8ca311ae3b1
michael mccune <msm@opbstudios.com>
2023-02-16 16:00:18 -0500

    UPSTREAM: <carry>: add machine api label and taint functionality
    This change re-adds the machine api support for labels and taints on
    node groups. The code was removed upstream as it is openshift specific,
    see this pull request[0].
    
    It also adds in the functionality of the upstream override annotation
    for labels and taints[1] to support
    https://issues.redhat.com/browse/MIXEDARCH-259
    
    [0]: https://github.com/kubernetes/autoscaler/pull/5249
    [1]: https://github.com/kubernetes/autoscaler/pull/5382

--- cluster-autoscaler/cloudprovider/clusterapi/clusterapi_nodegroup_test.go
+++ cluster-autoscaler/cloudprovider/clusterapi/clusterapi_nodegroup_test.go
@@ -1297,6 +1297,7 @@ func TestNodeGroupTemplateNodeInfo(t *testing.T) {
 
 	type testCaseConfig struct {
 		nodeLabels         map[string]string
+		nodegroupLabels    map[string]string
 		includeNodes       bool
 		expectedErr        error
 		expectedCapacity   map[corev1.ResourceName]int64
@@ -1352,6 +1353,10 @@ func TestNodeGroupTemplateNodeInfo(t *testing.T) {
 			},
 			config: testCaseConfig{
 				expectedErr: nil,
+				nodegroupLabels: map[string]string{
+					"nodeGroupLabel": "value",
+					"anotherLabel":   "anotherValue",
+				},
 				expectedCapacity: map[corev1.ResourceName]int64{
 					corev1.ResourceCPU:        2,
 					corev1.ResourceMemory:     2048 * 1024 * 1024,
@@ -1359,9 +1364,11 @@ func TestNodeGroupTemplateNodeInfo(t *testing.T) {
 					gpuapis.ResourceNvidiaGPU: 1,
 				},
 				expectedNodeLabels: map[string]string{
+					"kubernetes.io/hostname": "random value",
 					"kubernetes.io/os":       "linux",
 					"kubernetes.io/arch":     "arm64",
-					"kubernetes.io/hostname": "random value",
+					"nodeGroupLabel":         "value",
+					"anotherLabel":           "anotherValue",
 					"my-custom-label":        "custom-value",
 				},
 			},
@@ -1396,6 +1403,12 @@ func TestNodeGroupTemplateNodeInfo(t *testing.T) {
 	}
 
 	test := func(t *testing.T, testConfig *testConfig, config testCaseConfig) {
+		if testConfig.machineDeployment != nil {
+			unstructured.SetNestedStringMap(testConfig.machineDeployment.Object, config.nodegroupLabels, "spec", "template", "spec", "metadata", "labels")
+		} else {
+			unstructured.SetNestedStringMap(testConfig.machineSet.Object, config.nodegroupLabels, "spec", "template", "spec", "metadata", "labels")
+		}
+
 		if config.includeNodes {
 			for i := range testConfig.nodes {
 				testConfig.nodes[i].SetLabels(config.nodeLabels)
