3de6f10acc839be704ec59fe94854f639797e77b
Joel Speed <joel.speed@hotmail.co.uk>
2024-01-22 18:11:33 +0000

    UPSTREAM: <carry>: Fix unstructured taint parsing in Cluster API provider

--- cluster-autoscaler/cloudprovider/clusterapi/clusterapi_unstructured.go
+++ cluster-autoscaler/cloudprovider/clusterapi/clusterapi_unstructured.go
@@ -211,10 +211,10 @@ func (r unstructuredScalableResource) Taints() []apiv1.Taint {
 	}
 	if found {
 		for _, t := range newtaints {
-			if v, ok := t.(apiv1.Taint); ok {
-				taints = append(taints, v)
+			if t := unstructuredToTaint(t); t != nil {
+				taints = append(taints, *t)
 			} else {
-				klog.Warning("Unable to convert data to taint: %v", t)
+				klog.Warningf("Unable to convert of type %T data to taint: %+v", t, t)
 				continue
 			}
 		}
@@ -233,6 +233,19 @@ func (r unstructuredScalableResource) Taints() []apiv1.Taint {
 	}
 
 	return taints
+}
+
+func unstructuredToTaint(unstructuredTaintInterface interface{}) *corev1.Taint {
+	unstructuredTaint := unstructuredTaintInterface.(map[string]interface{})
+	if unstructuredTaint == nil {
+		return nil
+	}
+
+	taint := &corev1.Taint{}
+	taint.Key = unstructuredTaint["key"].(string)
+	taint.Value = unstructuredTaint["value"].(string)
+	taint.Effect = corev1.TaintEffect(unstructuredTaint["effect"].(string))
+	return taint
 }
 
 // A node group can scale from zero if it can inform about the CPU and memory
