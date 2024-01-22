8e00fc00f84c9a8bf0e9fa402201e056147a455c
Joel Speed <joel.speed@hotmail.co.uk>
2024-01-22 18:11:33 +0000

    UPSTREAM: <carry>: Fix unstructured taint parsing in Cluster API provider
    This change corrects the behavior for parsing taints from the
    unstructured scalable resource. This is required on OpenShift as our
    implementation is slightly different from the upstream.

--- cluster-autoscaler/cloudprovider/clusterapi/clusterapi_controller_test.go
+++ cluster-autoscaler/cloudprovider/clusterapi/clusterapi_controller_test.go
@@ -382,6 +382,17 @@ func createTestConfigs(specs ...testSpec) []*testConfig {
 								"kind":       machineTemplateKind,
 								"name":       "TestMachineTemplate",
 							},
+							"taints": []interface{}{
+								map[string]interface{}{
+									"key":    "test",
+									"value":  "test",
+									"effect": "NoSchedule",
+								},
+								map[string]interface{}{
+									"key":    "test-no-value",
+									"effect": "NoSchedule",
+								},
+							},
 						},
 					},
 				},
@@ -419,6 +430,17 @@ func createTestConfigs(specs ...testSpec) []*testConfig {
 									"apiGroup": "infrastructure.cluster.x-k8s.io",
 									"kind":     machineTemplateKind,
 									"name":     "TestMachineTemplate",
+								},
+								"taints": []interface{}{
+									map[string]interface{}{
+										"key":    "test",
+										"value":  "test",
+										"effect": "NoSchedule",
+									},
+									map[string]interface{}{
+										"key":    "test-no-value",
+										"effect": "NoSchedule",
+									},
 								},
 							},
 						},
